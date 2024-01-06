d739a55e618ef07b2ec01e40d0dda288bf5deaaa
Cris-casas-react <cristina.casas2@gmail.com>
2024-01-06 23:55:52 +0100

    new-task-button

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 let checkboxes = document.querySelectorAll('input[type="checkbox"]');
 let deleteButtons = document.querySelectorAll('button[value="delete"]');
 let editButtons = document.querySelectorAll('button[value="edit"]');
+let newTaskButton = document.getElementById('button-new-task');
+let formulario = document.getElementById('taskForm');
 
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
+newTaskButton.addEventListener('click', () => {
+    let labelNewTask = document.createElement('label');
+    labelNewTask.innerHTML = `
+    <input type="checkbox">
+    <input type="text">
+    <button value="edit">Edit</button>
+    <button value="delete">Delete</button>
+    `;
+    formulario.appendChild(labelNewTask);
+        
+});
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
         checkbox.nextElementSibling.style.textDecoration = checkbox.checked ? "line-through" : "none";
-        /*   
-        if(checkbox.checked){
-            text.style.textDecoration = "line-through";
-        } else {
-            text.style.textDecoration = "none";
-        }
-        */
     });
   });
 
 deleteButtons.forEach((button) => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', () => {
         button.parentElement.remove();
     });
   });
 
-  editButtons.forEach((button) => {
-    button.addEventListener('click', function() {
+editButtons.forEach((button) => {
+    button.addEventListener('click', () => {
         button.previousElementSibling.focus();
-    });
+});
 });
 
 
+/* newTaskButton.addEventListener('click', function() {
 
+    console.log('click button new task');
+    let labelNewTask = document.createElement('label');
+    labelNewTask.innerHTML = `
+    <input type="checkbox">
+    <input type="text">
+    <button value="edit">Edit</button>
+    <button value="delete">Delete</button>
+    `;
+    form.appendChild(labelNewTask);
 
-
-// checkboxes.addEventListener('click', cambiarEstilo);
+}); */
